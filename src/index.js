3ba037fa3761030da68ac3f621415b9dd39d77e4
C <earthhominid@gmail.com>
2022-04-13 14:51:55 -0300

    added routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import routes from './routes';
 import models from './models';
 import { v4 as uuidv4 } from 'uuid';
 
 const port = process.env.PORT;
 const app = express();
+
+app.use('/session', routes.session);
+app.use('users', routes.user);
+app.use('/messages', routes.message);
 
 app.use(cors());
 app.use(express.json());
@@ -17,48 +22,6 @@ app.use((req, res, next) => {
     me: models.users[1],
   };
   next();
-});
-
-app.get('/session', (req, res) => {
-  return res.send(req.context.models.users[req.context.me.id]);
-});
-
-app.get('/users', (req, res) => {
-  return res.send(Object.values(req.context.models.users));
-});
-
-app.get('/users/:userId', (req, res) => {
-  return res.send(req.context.models.users[req.params.userId]);
-});
-
-app.get('/messages', (req, res) => {
-  return res.send(Object.values(req.context.models.messages));
-});
-
-app.get('/messages/:messageId', (req, res) => {
-  return res.send(req.context.models.messages[req.params.messageId]);
-});
-
-app.post('/messages', (req, res) => {
-  const id = uuidv4();
-  const message = {
-    id,
-    text: req.body.text,
-    userId: req.context.me.id,
-  };
-
-  req.context.models.messages[id] = message;
-
-  return res.send(message);
-});
-
-app.delete('/messages/:messageId', (req, res) => {
-  const { [req.params.messageId]: message, ...otherMessages } =
-    req.context.models.messages;
-
-  req.context.models.messages = otherMessages;
-
-  return res.send(message);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
